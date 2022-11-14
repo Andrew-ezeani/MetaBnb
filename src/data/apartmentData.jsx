4ec4bff8b44be37f6ce3851bbe9347474e76cab0
Alwaysgreen18 <ezeaniiandrew@gmail.com>
2022-11-14 03:44:15 +0100

    created NFT's apartment data

---
+++ src/data/apartmentData.jsx
@@ -0,0 +1,165 @@
+import frame1 from "../assets/images/Frame 1.png";
+import frame2 from "../assets/images/Frame 2.png";
+import frame3 from "../assets/images/Frame 3.png";
+import frame4 from "../assets/images/Frame 4.png";
+import frame5 from "../assets/images/Frame 5.png";
+import frame6 from "../assets/images/Frame 6.png";
+import frame7 from "../assets/images/Frame 7.png";
+import frame8 from "../assets/images/Frame 8.png";
+import frame9 from "../assets/images/Frame 9.png";
+import frame10 from "../assets/images/Frame 10.png";
+import frame11 from "../assets/images/Frame 11.png";
+import frame12 from "../assets/images/Frame 12.png";
+import frame13 from "../assets/images/Frame 13.png";
+import frame14 from "../assets/images/Frame 14.png";
+import frame15 from "../assets/images/Frame 15.png";
+import frame16 from "../assets/images/Frame 16.png";
+
+const apartmentData = [
+  {
+    id: 1,
+    poster_img: frame1,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 2,
+    poster_img: frame2,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 3,
+    poster_img: frame3,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 4,
+    poster_img: frame4,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 5,
+    poster_img: frame5,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 6,
+    poster_img: frame6,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 7,
+    poster_img: frame7,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 8,
+    poster_img: frame8,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 9,
+    poster_img: frame9,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 10,
+    poster_img: frame10,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 11,
+    poster_img: frame11,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 12,
+    poster_img: frame12,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 13,
+    poster_img: frame13,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 14,
+    poster_img: frame14,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 15,
+    poster_img: frame15,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+  {
+    id: 16,
+    poster_img: frame16,
+    name: "Desert king",
+    price: "1MBT per night",
+    distance: "2345km away",
+    duration: "available for 2weeks stay",
+    rating: 5,
+  },
+];
+
+export default apartmentData;
