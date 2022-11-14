369d53ef56b9db9e5143eebc9d17c895716c13ed
Alwaysgreen18 <ezeaniiandrew@gmail.com>
2022-11-14 14:29:35 +0100

    minor code refactoring

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,41 +1,23 @@
 import React from "react";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Hero from "./components/Hero";
 import Wallets from "./components/Wallets";
-import ShowApartments from "../../components/ShowApartments";
-import apartmentData from "../../data/apartmentData";
+import DisplayApartments from "../../components/DisplayApartments";
+import Container from "@mui/material/Container";
 
 const Home = () => {
-  const apartments = apartmentData.slice(0, 8);
-
   return (
     <>
       <Container
+        maxWidth={false}
         sx={{
+          maxWidth: "100vw",
           marginTop: "4em",
         }}
       >
         <Hero />
       </Container>
       <Wallets />
-      <Container>
-        <Grid container spacing={5}>
-          {apartments.map((apartment) => {
-            return (
-              <ShowApartments
-                isSlice={true}
-                key={apartment.id}
-                img={apartment.poster_img}
-                name={apartment.name}
-                price={apartment.price}
-              />
-            );
-          })}
-
-          <ShowApartments />
-        </Grid>
-      </Container>
+      <DisplayApartments isSlice={true} />
     </>
   );
 };
