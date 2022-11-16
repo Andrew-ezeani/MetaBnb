1407cc0250ae5d8d3c9b0f5d7b1b366f46270727
Alwaysgreen18 <ezeaniiandrew@gmail.com>
2022-11-16 01:33:39 +0100

    changed routing path

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route path="/Metabnb/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="places" element={<Places />} />
           <Route path="community" element={<Community />} />
